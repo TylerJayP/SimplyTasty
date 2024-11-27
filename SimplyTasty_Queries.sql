19fd754716ac12a54bc1a9d6534a647f89dd65c3
Tyler Perkins <tylerjayp12@gmail.com>
2024-11-26 17:33:13 -0700

    Added a query file for us to run example queries for our video. I have also removed all the unnecessary files from the github repo to keep things consistent and clean.

---
+++ SimplyTasty_Queries.sql
@@ -0,0 +1,18 @@
+USE SimplyTasty;
+
+SELECT * FROM airshipment;
+SELECT * FROM customer;
+SELECT * FROM customer_dietarypreference;
+SELECT * FROM customer_orderhistory;
+SELECT * FROM dietaryoptions;
+SELECT * FROM ingredient;
+SELECT * FROM inventory;
+SELECT * FROM landshipment;
+SELECT * FROM mealkit;
+SELECT * FROM mealkit_ingredients;
+SELECT * FROM mealkit_orders;
+SELECT * FROM orders;
+SELECT * FROM recipe;
+SELECT * FROM recipe_dietaryoptions;
+SELECT * FROM recipe_ingredients;
+SELECT * FROM shipments;
